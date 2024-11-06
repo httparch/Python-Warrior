8a82a4c05a968fea4a928e5fc6fd5e4ec1ec4dbd
httparch <shanebigornia123@gmail.com>
2024-11-06 23:11:03 +0800

    fix skill Expertise

--- assets/js/console.js
+++ assets/js/console.js
@@ -1,5 +1,5 @@
 ;(function initConsoleLogDiv() {
-  'use strict'
+  "use strict"
 
   if (console.log.toDiv) {
     return
@@ -9,53 +9,57 @@
     if (x instanceof Error) {
       return x.message
     }
-    return typeof x === 'string' ? x : JSON.stringify(x)
+    return typeof x === "string" ? x : JSON.stringify(x)
   }
 
   var log = console.log.bind(console)
   var error = console.error.bind(console)
   var warn = console.warn.bind(console)
   var table = console.table ? console.table.bind(console) : null
-  var id = 'console-log-div'
+  var id = "console-log-div"
 
   function createOuterElement() {
     var outer = document.getElementById(id)
     if (!outer) {
-      outer = document.createElement('fieldset')
+      outer = document.createElement("fieldset")
       outer.id = id
       document.body.appendChild(outer)
     }
     outer.classList.add(id)
 
     var style = outer.style
-    style.fontFamily = 'monospace'
-    style.marginLeft = 'auto'
-    style.marginRight = 'auto'
-    style.marginBottom = '10px'
-    style.whiteSpace = 'pre'
-    style.width = '940px'
-    style.padding = '5px 10px'
+    style.fontFamily = "monospace"
+    style.marginLeft = "auto"
+    style.marginRight = "auto"
+    style.marginBottom = "10px"
+    style.whiteSpace = "pre"
+    style.width = "940px"
+    style.padding = "5px 10px"
     return outer
   }
 
   var logTo = (function createLogDiv() {
     var outer = createOuterElement()
 
-    var legend = document.createElement('legend')
+    var legend = document.createElement("legend")
     outer.appendChild(legend)
 
-    var div = document.createElement('div')
-    div.id = 'console-log-text'
+    var div = document.createElement("div")
+    div.id = "console-log-text"
     outer.appendChild(div)
 
     return div
   })()
 
   function printToDiv() {
-    var msg = Array.prototype.slice.call(arguments, 0).map(toString).join(' ')
-    if (!msg.includes('Original Python:') && !msg.includes('Translated code:') && !msg.includes('Converted JavaScript:')) {
-        var text = logTo.textContent
-        logTo.textContent = text + msg + '\n'
+    var msg = Array.prototype.slice.call(arguments, 0).map(toString).join(" ")
+    if (
+      !msg.includes("Original Python:") &&
+      !msg.includes("Translated code:") &&
+      !msg.includes("Converted JavaScript:")
+    ) {
+      var text = logTo.textContent
+      logTo.textContent = text + msg + "\n"
     }
   }
 
@@ -70,71 +74,75 @@
   console.error = function errorWithCopy() {
     error.apply(null, arguments)
     var args = Array.prototype.slice.call(arguments, 0)
-    args.unshift('ERROR:')
+    args.unshift("ERROR:")
     printToDiv.apply(null, args)
   }
 
   console.warn = function logWarning() {
     warn.apply(null, arguments)
     var args = Array.prototype.slice.call(arguments, 0)
-    args.unshift('WARNING:')
+    args.unshift("WARNING:")
     printToDiv.apply(null, args)
   }
 
   function printTable(objArr, keys) {
     var numCols = keys.length
     var len = objArr.length
-    var $table = document.createElement('table')
-    $table.style.width = '100%'
-    $table.setAttribute('border', '1')
-    var $head = document.createElement('thead')
-    var $tdata = document.createElement('td')
-    $tdata.innerHTML = 'Index'
+    var $table = document.createElement("table")
+    $table.style.width = "100%"
+    $table.setAttribute("border", "1")
+    var $head = document.createElement("thead")
+    var $tdata = document.createElement("td")
+    $tdata.innerHTML = "Index"
     $head.appendChild($tdata)
 
     for (var k = 0; k < numCols; k++) {
-      $tdata = document.createElement('td')
+      $tdata = document.createElement("td")
       $tdata.innerHTML = keys[k]
       $head.appendChild($tdata)
     }
     $table.appendChild($head)
 
     for (var i = 0; i < len; i++) {
-      var $line = document.createElement('tr')
-      $tdata = document.createElement('td')
+      var $line = document.createElement("tr")
+      $tdata = document.createElement("td")
       $tdata.innerHTML = i
       $line.appendChild($tdata)
 
       for (var j = 0; j < numCols; j++) {
-        $tdata = document.createElement('td')
+        $tdata = document.createElement("td")
         $tdata.innerHTML = objArr[i][keys[j]]
         $line.appendChild($tdata)
       }
       $table.appendChild($line)
     }
-    var div = document.getElementById('console-log-text')
+    var div = document.getElementById("console-log-text")
     div.appendChild($table)
   }
 
   console.table = function logTable() {
-    if (typeof table === 'function') {
+    if (typeof table === "function") {
       table.apply(null, arguments)
     }
 
     var objArr = arguments[0]
     var keys
 
-    if (typeof objArr[0] !== 'undefined') {
+    if (typeof objArr[0] !== "undefined") {
       keys = Object.keys(objArr[0])
     }
     printTable(objArr, keys)
   }
 
-  window.addEventListener('error', function (err) {
-    if (err.message.includes('eval')) {
-        printToDiv("There's something wrong with your code")
+  window.addEventListener("error", function (err) {
+    if (err.message.includes("eval")) {
+      printToDiv("There's something wrong with your code")
     } else {
-        printToDiv('EXCEPTION:', err.message + '\n  ' + err.filename, err.lineno + ':' + err.colno)
+      printToDiv(
+        "EXCEPTION:",
+        err.message + "\n  " + err.filename,
+        err.lineno + ":" + err.colno
+      )
     }
   })
 })()
